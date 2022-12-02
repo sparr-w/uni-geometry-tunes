894d0b0deec236387d7c5393609634360790f044
sparr-w <redowag@gmail.com>
2022-12-02 16:45:40 +0000

    spinning shooter implementation
    further implementation, optimisation, modulation

--- Assets/Enemies/Scripts/Projectile.cs
+++ Assets/Enemies/Scripts/Projectile.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-    public float Speed = 1.0f;
+    private float speed = 1.0f;
+
+    public Projectile Init(float speed = 1.0f) {
+        this.speed = speed;
+        return this;
+    }
 
     private void Move(float distance) {
         this.transform.position += transform.up * distance;
@@ -14,6 +19,6 @@ public class Projectile : MonoBehaviour {
     }
 
     private void Update() {
-        Move(Speed * Time.deltaTime);
+        Move(speed * Time.deltaTime);
     }
 }
--- Assets/Enemies/Scripts/ShooterBehaviour.cs
+++ Assets/Enemies/Scripts/ShooterBehaviour.cs
@@ -1,33 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShooterBehaviour : MonoBehaviour {
+    public bool isFiring = true;
+    
+    [Header("Shooter Behaviour")]
+    public float RotationSpeed = 0.1f;
+    
+    [Header("Shooting Behaviour")]
     public Projectile ProjectileType;
     public float ProjectileSpeed = 3.5f;
     public Vector2 ProjectileScale = new Vector2(.3f, .3f);
     public float ShotDelay = 0.2f;
-    public bool isFiring = true;
+    [SerializeField] private bool shootLeft, shootRight, shootUp, shootDown;
 
     void Start() {
         StartCoroutine(Shoot());
+        
+        if (shootUp) transform.GetChild(0).gameObject.SetActive(true);
+        else transform.GetChild(0).gameObject.SetActive(false);
+        
+        if (shootLeft) transform.GetChild(1).gameObject.SetActive(true);
+        else transform.GetChild(1).gameObject.SetActive(false);
+        
+        if (shootDown) transform.GetChild(2).gameObject.SetActive(true);
+        else transform.GetChild(2).gameObject.SetActive(false);
+        
+        if (shootRight) transform.GetChild(3).gameObject.SetActive(true);
+        else transform.GetChild(3).gameObject.SetActive(false);
     }
 
     private IEnumerator Shoot() {
         yield return 0;
         
         while (isFiring) {
-            Projectile proj = Instantiate(ProjectileType);
-            proj.transform.rotation = transform.rotation;
-            proj.transform.localScale = new Vector3(ProjectileScale.x, ProjectileScale.y, 1.0f);
-            proj.Speed = ProjectileSpeed;
+            if (shootUp) FireProjectile(new Vector3(0.0f, 0.5f, 0.0f));
+            if (shootLeft) FireProjectile(new Vector3(-0.5f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 90.0f));
+            if (shootDown) FireProjectile(new Vector3(0.0f, -0.5f, 0.0f), new Vector3(0.0f, 0.0f, 180.0f));
+            if (shootRight) FireProjectile(new Vector3(0.5f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 270.0f));
 
             yield return new WaitForSeconds(ShotDelay);
         }
     }
 
+    private Projectile FireProjectile(Vector3? localPos = null, Vector3? localRot = null) {
+        if (localPos == null) localPos = new Vector3(0.0f, 0.0f, 0.0f);
+        if (localRot == null) localRot = new Vector3(0.0f, 0.0f, 0.0f);
+
+        Projectile proj = Instantiate(ProjectileType, this.transform);
+        proj.Init(ProjectileSpeed);
+        
+        proj.transform.localPosition = localPos.Value;
+        proj.transform.localEulerAngles = localRot.Value;
+
+        proj.transform.SetParent(null);
+        proj.transform.localScale = new Vector3(ProjectileScale.x, ProjectileScale.y, 1.0f);
+
+        return proj;
+    }
+
     private void Move() {
-        this.transform.eulerAngles += Vector3.forward * 0.1f;
+        this.transform.eulerAngles += Vector3.forward * RotationSpeed;
     }
 
     void Update() {
