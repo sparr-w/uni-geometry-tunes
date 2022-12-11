49eae32086d372aa33bd1b970c9784d554d5cc92
sparrow <redowag@gmail.com>
2022-12-11 23:55:44 +0000

    laser now grows

--- Assets/Enemies/Scripts/Laser.cs
+++ Assets/Enemies/Scripts/Laser.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : Projectile {
+    [SerializeField] private float finalWidth = 0.5f;
+
+    private Transform beamComponent;
     private Vector2 startPos;
+    
+    private float chargeProgress = 0.0f;
+
+    private void Start() {
+        beamComponent = this.transform.GetChild(0); // this should get the expanding beam part if the structure isn't tampered with
+    }
     
     // appear during initialisation, in the correct position
     public Laser Init(Vector2 _startPos) {
@@ -12,8 +21,25 @@ public class Laser : Projectile {
     }
     
     // expand in size, becoming more opaque and clear it is going to "blast"
+    private bool Charge(float increment) {
+        chargeProgress += increment;
+        chargeProgress = chargeProgress > 1.0f ? 1.0f : chargeProgress;
+
+        float newWidth = finalWidth * chargeProgress;
+        beamComponent.localScale = new Vector3(beamComponent.localScale.x,
+            newWidth, beamComponent.localScale.z);
+
+        return chargeProgress >= 1.0f;
+    }
     
     // "blast", everything disappears briefly before a solid beam appears, dealing damage
+    private void Shoot() {
+        
+    }
     
     // the beam should stick the barrel it is firing from, so it may need to move with owner
+
+    private void Update() {
+        if (!Charge(Speed * Time.deltaTime)) ;
+    }
 }
