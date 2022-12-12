09f77dd548a2f982334385894e11eb59d07aa172
sparrow <redowag@gmail.com>
2022-12-12 00:10:01 +0000

    ok, now beam fully charges then shoots

--- Assets/Enemies/Scripts/Laser.cs
+++ Assets/Enemies/Scripts/Laser.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Laser : Projectile {
     [SerializeField] private float finalWidth = 0.5f;
+    [SerializeField] private float finalOpacity = 0.5f;
 
     private Transform beamComponent;
+    private SpriteRenderer beamRendererComponent;
     private Vector2 startPos;
     
     private float chargeProgress = 0.0f;
 
     private void Start() {
         beamComponent = this.transform.GetChild(0); // this should get the expanding beam part if the structure isn't tampered with
+        beamRendererComponent = beamComponent.GetComponent<SpriteRenderer>();
+        
+        beamRendererComponent.color = new Color(beamRendererComponent.color.r,
+            beamRendererComponent.color.g,
+            beamRendererComponent.color.b,
+            0.0f); // zero out the alpha by default
     }
     
     // appear during initialisation, in the correct position
@@ -29,12 +37,19 @@ public class Laser : Projectile {
         beamComponent.localScale = new Vector3(beamComponent.localScale.x,
             newWidth, beamComponent.localScale.z);
 
+        float newOpacity = finalOpacity * chargeProgress;
+        beamRendererComponent.color = new Color(beamRendererComponent.color.r,
+            beamRendererComponent.color.g,
+            beamRendererComponent.color.b,
+            newOpacity);
+        
+        if (chargeProgress >= 1.0f) Shoot();
         return chargeProgress >= 1.0f;
     }
     
     // "blast", everything disappears briefly before a solid beam appears, dealing damage
     private void Shoot() {
-        
+        beamRendererComponent.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
     }
     
     // the beam should stick the barrel it is firing from, so it may need to move with owner
